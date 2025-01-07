b9171c03373fb9716756233edef895abd2dcca2f
Parkchaeyeon <ccyy1029@naver.com>
2025-01-08 03:24:19 +0900

    chore: reset 파일 내용 수정

--- src/styles/reset.css.ts
+++ src/styles/reset.css.ts
@@ -1,12 +1,70 @@
 import { globalStyle } from '@vanilla-extract/css';
 import { reset } from '@styles/layers.css';
 
-/*
- * 'display' 속성만 제외한 모든 "User-Agent-StyleSheet" 스타일을 제거합니다.
- * - "symbol *" 부분은 Firefox에서 발생하는 SVG 스프라이트 버그를 해결하기 위한 것입니다.
- * - "html" 요소는 제외되며, 그렇지 않으면 Chrome에서 CSS 하이픈(hyphens) 속성이 망가지는 버그가 발생합니다.
- *   (관련 문제: https://github.com/elad2412/the-new-css-reset/issues/36).
- */
+globalStyle('*', {
+  '@layer': {
+    [reset]: {
+      margin: '0',
+      padding: '0',
+      border: '0',
+      fontSize: '100%',
+      font: 'inherit',
+      verticalAlign: 'baseline',
+    },
+  },
+});
+
+globalStyle(
+  'article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section',
+  {
+    '@layer': {
+      [reset]: {
+        display: 'block',
+      },
+    },
+  }
+);
+
+globalStyle('body', {
+  '@layer': {
+    [reset]: {
+      lineHeight: '1',
+    },
+  },
+});
+
+globalStyle('ol, ul', {
+  '@layer': {
+    [reset]: {
+      listStyle: 'none',
+    },
+  },
+});
+
+globalStyle('blockquote, q', {
+  '@layer': {
+    [reset]: {
+      quotes: 'none',
+    },
+  },
+});
+
+globalStyle('blockquote:before, blockquote:after, q:before, q:after', {
+  '@layer': {
+    [reset]: {
+      content: '',
+    },
+  },
+});
+
+globalStyle('table', {
+  '@layer': {
+    [reset]: {
+      borderCollapse: 'collapse',
+      borderSpacing: '0',
+    },
+  },
+});
 
 globalStyle(
   '*:where(:not(html, iframe, canvas, img, svg, video, audio):not(svg *, symbol *))',
@@ -79,164 +137,6 @@ globalStyle('img', {
     [reset]: {
       maxInlineSize: '100%',
       maxBlockSize: '100%',
-    },
-  },
-});
-
-/**
- * 테이블 셀 사이의 기본 간격을 제거
- * Removes spacing between cells in tables
- */
-globalStyle('table', {
-  '@layer': {
-    [reset]: {
-      borderCollapse: 'collapse',
-    },
-  },
-});
-
-/**
- * 사파리 브라우저에서 user-select:none을 적용할 때 발생할 수 있는 문제를 방지하고, 텍스트 입력 요소가 정상적으로 동작
- * Safari - solving issue when using user-select:none on the <body> text input
- * doesn't working
- */
-globalStyle('input, textarea', {
-  '@layer': {
-    [reset]: {
-      WebkitUserSelect: 'auto',
-    },
-  },
-});
-
-/**
- * 사파리 브라우저에서 textarea 요소의 white-space 속성을 기본값으로 되돌리기 위해 사용됩니다.
- * Revert the 'white-space' property for textarea elements on Safari
- */
-globalStyle('textarea', {
-  '@layer': {
-    [reset]: {
-      whiteSpace: 'revert',
-    },
-  },
-});
-
-/**
- * meter 태그 사용을 위한 최소한의 스타일 설정
- * Minimum style to allow to style meter element
- */
-globalStyle('meter', {
-  '@layer': {
-    [reset]: {
-      WebkitAppearance: 'revert',
-      appearance: 'revert',
-    },
-  },
-});
-
-/**
- * pre 태그의 브라우저 기본 스타일을 복원, box-sizing border-box 설정
- * Preformatted text - use only for this feature
- */
-globalStyle(':where(pre)', {
-  '@layer': {
-    [reset]: {
-      all: 'revert',
-      boxSizing: 'border-box',
-    },
-  },
-});
-
-/**
- * input의 placeholder의 컬러를 지정하지 않음
- * Reset default text opacity of input placeholder
- */
-globalStyle('::placeholder', {
-  '@layer': {
-    [reset]: {
-      color: 'unset',
-    },
-  },
-});
-
-/**
- * hidden 속성을 가진 요소의 display none을 적용
- * Fix the feature of 'hidden' attribute. display:revert; revert to element
- * instead of attribute
- */
-globalStyle(':where([hidden])', {
-  '@layer': {
-    [reset]: {
-      display: 'none',
-    },
-  },
-});
-
-/**
- * contenteditable 요소의 편집 기능이 제대로 동작하도록 설정
- * Revert for bug in Chromium browsers
- *
- * - Fix for the content editable attribute will work properly.
- * - Webkit-user-select: auto; added for Safari in case of using user-select:none
- *   on wrapper element
- */
-globalStyle(':where([contenteditable]:not([contenteditable="false"]))', {
-  // @ts-expect-error: -webkit-line-break is a non-standard property
-  '@layer': {
-    [reset]: {
-      MozUserModify: 'read-write',
-      WebkitUserModify: 'read-write',
-      overflowWrap: 'break-word',
-      WebkitLineBreak: 'after-white-space',
-      WebkitUserSelect: 'auto',
-    },
-  },
-});
-
-/**
- * draggable 속성이 있는 요소에서 드래그 기능이 제대로 작동하도록 설정
- * Apply back the draggable feature - exist only in Chromium and Safari
- */
-globalStyle(':where([draggable="true"])', {
-  '@layer': {
-    [reset]: {
-      // @ts-expect-error: -webkit-user-drag is a non-standard property
-      WebkitUserDrag: 'element',
-    },
-  },
-});
-
-/**
- * modal의 기본 동작 복원
- * Revert Modal native behavior
- */
-globalStyle(':where(dialog:modal)', {
-  '@layer': {
-    [reset]: {
-      all: 'revert',
-      boxSizing: 'border-box',
-    },
-  },
-});
-
-/**
- * input[type="number"]의 스핀 버튼 제거
- * Remove the spin button for number input
- */
-
-globalStyle('input[type="number"]::-webkit-outer-spin-button', {
-  '@layer': {
-    [reset]: {
-      WebkitAppearance: 'none',
-      margin: '0',
-    },
-  },
-});
-
-globalStyle('input[type="number"]::-webkit-inner-spin-button', {
-  '@layer': {
-    [reset]: {
-      WebkitAppearance: 'none',
-      margin: '0',
     },
   },
 });
