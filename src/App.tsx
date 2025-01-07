86cccede347a893b20ae4888bf8979b598f8e1bc
imhwarang <imhwarang4@naver.com>
2025-01-07 20:16:35 +0900

    init: tanstack-query, axios 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import queryClient from './queryClient';
+
 const App = () => {
-  return <p>cakey web 화이팅!!!!</p>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <p>cakey web 화이팅!!!!</p>;
+      <div style={{ fontSize: '16px' }}>
+        <ReactQueryDevtools />
+      </div>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
