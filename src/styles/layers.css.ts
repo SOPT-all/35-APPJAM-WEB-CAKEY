fb1efc8cdbed5951c3b1b148b6905e592264ad24
Parkchaeyeon <ccyy1029@naver.com>
2025-01-08 05:12:07 +0900

    feat: theme 세팅하기

--- src/styles/layers.css.ts
+++ src/styles/layers.css.ts
@@ -2,5 +2,3 @@ import { layer } from '@vanilla-extract/css';
 
 export const reset = layer('reset');
 export const base = layer('base');
-export const components = layer('components');
-export const utilities = layer('utilities');
