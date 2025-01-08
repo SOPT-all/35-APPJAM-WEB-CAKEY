5af2f66831b01f0b53e655c41fa00c4ae177edb9
thisishwarang <101498590+thisishwarang@users.noreply.github.com>
2025-01-08 23:59:51 +0900

    [Init/#11] tanstack-query, axios 초기 세팅 (#14)
    * init: tanstack-query, axios 초기 세팅
    
    * init: withCredentials: true 옵션 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import queryClient from './queryClient';
 import '@styles/global.css';
 
 const App = () => {
-  return <p>cakey web 화이팅!!!!</p>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <p>cakey web 화이팅!!!!</p>;
+      <div style={{ fontSize: '16px' }}>
+        <ReactQueryDevtools />
+      </div>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
