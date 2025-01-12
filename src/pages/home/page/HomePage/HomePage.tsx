85b2b77980592fe699c4bcc9aacb4271b8ce20bf
imhwarang <imhwarang4@naver.com>
2025-01-13 02:30:07 +0900

    fix: HomePage 컴포넌트 원상복귀

--- src/pages/home/page/HomePage/HomePage.tsx
+++ src/pages/home/page/HomePage/HomePage.tsx
@@ -1,12 +1,5 @@
-import { IconButton } from '@components';
-
 const HomePage = () => {
-  return (
-    <div style={{ backgroundColor: 'black' }}>
-      <IconButton buttonType="save" onMap isActive={false} />
-      <IconButton buttonType="like" onMap={false} />
-    </div>
-  );
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
