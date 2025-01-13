8eb5adb79f5bb342a35de3a320965aacee6f770a
thisishwarang <101498590+thisishwarang@users.noreply.github.com>
2025-01-14 00:51:02 +0900

    [Feat/#53] FilteringButton 공통 컴포넌트 구현 (#58)
    * feat: FilteringButton 컴포넌트 초기 구현
    
    * feat: FilteringButton 컴포넌트 구현 완료
    
    * feat: dropdown 영역 외부를 클릭했을 때 dropdown 닫히도록 수정
    
    * fix: 코드리뷰 반영

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,3 +1,5 @@
+export { default as IcArrowDown20 } from './IcArrowDown20';
+export { default as IcArrowUp20 } from './IcArrowUp20';
 export { default as IcKakao } from './IcKakao';
 export { default as IcMy } from './IcMy';
 export { default as ImgLogo } from './ImgLogo';
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -5,5 +5,6 @@ import Tab from './Tab/Tab';
 import Header from './Header/Header';
 import CenterModal from './CenterModal/CenterModal';
 import BottomModal from './BottomModal/BottomModal';
+import FilteringButton from './FilteringButton/FilteringButton';
 
-export { Label, SocialLoginButton, Tab, TextButton, Header, CenterModal, BottomModal };
+export { Label, SocialLoginButton, Tab, TextButton, Header, CenterModal, BottomModal, FilteringButton };
