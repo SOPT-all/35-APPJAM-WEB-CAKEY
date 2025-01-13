4aef4d249ac07e8336bdc1ee4ffe1f0d07f20977
Parkchaeyeon <ccyy1029@naver.com>
2025-01-13 23:48:01 +0900

    chore: 사용하지 않는 예시 Button 컴포넌트 삭제

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,4 @@
-import { Button } from 'src/components/common/Button/Button';
 import SocialLoginButton from 'src/components/common/SocialLoginButton/SocialLoginButton';
 import TextButton from 'src/components/common/TextButton/TextButton';
 
-export { Button, SocialLoginButton, TextButton };
+export { SocialLoginButton, TextButton };
