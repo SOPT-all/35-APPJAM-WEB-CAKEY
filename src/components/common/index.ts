b6b4a649f30f557cea7600d1c092330a5433c239
chaeneey <127743435+chaeneey@users.noreply.github.com>
2025-01-14 00:45:03 +0900

    [Feat/#36] CenterModal 컴포넌트 구현 (#54)
    * feat: CenterModal 컴포넌트 모달 형태 구현
    
    * feat: useModal 만들어서 모달 열고 닫는 로직 관리하기
    
    * feat: backdrop 클릭 시 close 되도록 backdropClose prop 받기
    
    * chore: z-index 컨벤션 맞춰서 수정
    
    * refactor: 스타일 파일 import 할 때, 상대경로로 수정!
    
    ---------
    
    Co-authored-by: thisishwarang <101498590+thisishwarang@users.noreply.github.com>

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,5 +3,6 @@ import TextButton from "./TextButton/TextButton";
 import Label from './Label/Label';
 import Tab from './Tab/Tab';
 import Header from './Header/Header';
+import CenterModal from './CenterModal/CenterModal';
 
-export { Label, SocialLoginButton, Tab, TextButton, Header };
+export { Label, SocialLoginButton, Tab, TextButton, Header, CenterModal };
