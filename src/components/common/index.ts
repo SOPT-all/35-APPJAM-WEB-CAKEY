6f1cbd4f761ce52d5149e1a163a31d9ea85b2947
Parkchaeyeon <ccyy1029@naver.com>
2025-01-13 23:51:15 +0900

    chore: index 파일에 BottomModal 추가하기

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,6 @@
 import { Button } from 'src/components/common/Button/Button';
 
+import BottomModal from './BottomModal/BottomModal';
 import SocialLoginButton from './SocialLoginButton/SocialLoginButton';
 
-export { Button, SocialLoginButton };
+export { Button, SocialLoginButton, BottomModal };
