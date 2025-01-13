0e2f95884fcb62ed734a0bbd65e49fada39fc638
youtheyeon <127864650+youtheyeon@users.noreply.github.com>
2025-01-14 00:27:39 +0900

    [Feat/#43] Header 컴포넌트 구현 (#51)
    * feat: fixedGenerator() 생성
    
    * feat: Header 공통 컴포넌트 구현
    
    * design: 로고 변경 및 theme 반영
    
    * fix: Header defaultVariants 수정
    
    * feat: Header bgColor 기본값 지정
    
    * fix: defaultVariants 삭제
    
    ---------
    
    Co-authored-by: thisishwarang <101498590+thisishwarang@users.noreply.github.com>

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,1 +1,3 @@
 export { default as IcKakao } from './IcKakao';
+export { default as IcMy } from './IcMy';
+export { default as ImgLogo } from './ImgLogo';
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,5 +2,6 @@ import SocialLoginButton from "./SocialLoginButton/SocialLoginButton";
 import TextButton from "./TextButton/TextButton";
 import Label from './Label/Label';
 import Tab from './Tab/Tab';
+import Header from './Header/Header';
 
-export { Label, SocialLoginButton, Tab, TextButton };
+export { Label, SocialLoginButton, Tab, TextButton, Header };
