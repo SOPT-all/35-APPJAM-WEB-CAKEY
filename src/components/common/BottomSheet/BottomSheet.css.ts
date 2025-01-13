173f7ad0759d4973aec9d9e5e8656a0ab0e0a0f0
youtheyeon <yuseoyeon@ewhain.net>
2025-01-14 01:01:27 +0900

    feat: BottomSheet fixedGenerator() / z-index 적용

--- src/components/common/BottomSheet/BottomSheet.css.ts
+++ src/components/common/BottomSheet/BottomSheet.css.ts
@@ -1,31 +1,26 @@
 import { style } from '@vanilla-extract/css';
 import { vars } from '@styles/theme.css';
+import { fixedGenerator } from '@styles/generator.css';
 
-export const overlay = style({
-  position: 'fixed',
-  top: 0,
-  zIndex: 1,
-  width: '100%',
-  maxWidth: 'var(--max-width)',
-  minWidth: 'var(--min-width)',
-  height: 'calc(100dvh - 11.2rem)',
-});
+export const overlay = style([
+  fixedGenerator({ top: 0 }, 1),
+  {
+    height: 'calc(100dvh - 11.2rem)',
+  },
+]);
 
-export const container = style({
-  position: 'fixed',
-  bottom: 0,
-  zIndex: 2,
-  width: '100%',
-  maxWidth: 'var(--max-width)',
-  minWidth: 'var(--min-width)',
-  height: '80vh',
-  padding: '1.4rem 2rem 0',
+export const container = style([
+  fixedGenerator({ bottom: 0 }, 2),
+  {
+    height: '80vh',
+    padding: '1.4rem 2rem 0',
 
-  backgroundColor: vars.colors.white,
-  borderRadius: '2rem 2rem 0rem 0rem',
-  boxShadow: '0 0 2rem 0 rgba(0, 0, 0, 0.10)',
-  willChange: 'transform',
-});
+    backgroundColor: vars.colors.white,
+    borderRadius: '2rem 2rem 0rem 0rem',
+    boxShadow: '0 0 2rem 0 rgba(0, 0, 0, 0.10)',
+    willChange: 'transform',
+  },
+]);
 
 export const handler = style({
   margin: '0 auto',
