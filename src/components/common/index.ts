bab9d5ce4416dc3b2b74c4cee07dd173041e578a
쥬먐 <jiyoo0315@naver.com>
2025-01-14 00:17:20 +0900

    [Feat/#39] tab componants 구현 (#46)
    * feat: 작업 전 파일 생성
    
    * feat: MainTab 구현
    
    * chore: 파일 및 변수명 변경
    
    * feat: StoreTab 구현
    
    * feat: StoreTab 스타일 지정
    
    * chore: test용 코드 수정
    
    * chore: Tab 컴포넌트 variant 위해서 대대적 수정 시작
    
    * feat: Tab 컴포넌트 수정
    
    * feat: tab 컴포넌트 구현 완료
    
    * feat: index.ts에 컴포넌트 import
    
    * chore: 스타일 및 prop 관련 코드 리뷰 반영
    
    ---------
    
    Co-authored-by: thisishwarang <101498590+thisishwarang@users.noreply.github.com>

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,5 +2,7 @@ import { Button } from 'src/components/common/Button/Button';
 
 import Label from './Label/Label';
 import SocialLoginButton from './SocialLoginButton/SocialLoginButton';
+import Tab from './Tab/Tab';
 
-export { Button, Label, SocialLoginButton };
+
+export { Button, Label, SocialLoginButton, Tab };
