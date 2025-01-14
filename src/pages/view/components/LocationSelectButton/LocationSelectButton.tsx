83109aea4cf02ce19b9b61d5cfc8a48e24ff94c0
imhwarang <imhwarang4@naver.com>
2025-01-15 00:48:37 +0900

    feat: LocationSelectButton 컴포넌트 구현 완료

---
+++ src/pages/view/components/LocationSelectButton/LocationSelectButton.tsx
@@ -0,0 +1,28 @@
+import { IcCheck } from '@svgs';
+import { ButtonHTMLAttributes } from 'react';
+import { buttonStyle, buttonTextStyle } from './LocationSelectButton.css';
+
+export interface LocationSelectButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+  location: string;
+  currentLocation: string;
+}
+
+const LocationSelectButton = ({
+  location,
+  currentLocation,
+  onClick,
+}: LocationSelectButtonProps) => {
+  const isSelected = location === currentLocation;
+
+  return (
+    <button className={buttonStyle({ isSelected })} onClick={onClick}>
+      <span className={buttonTextStyle[isSelected ? 'true' : 'false']}>
+        {location}
+      </span>
+      {isSelected && <IcCheck width={28} height={28} />}
+    </button>
+  );
+};
+
+export default LocationSelectButton;
