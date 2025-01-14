aff6f546c52a31d4628321973170a5510688f7f2
imhwarang <imhwarang4@naver.com>
2025-01-14 19:24:30 +0900

    fix: 코드드리뷰 반영

--- src/pages/home/page/HomePage/HomePage.tsx
+++ src/pages/home/page/HomePage/HomePage.tsx
@@ -1,14 +1,5 @@
-import { IconButton } from '@components';
-
 const HomePage = () => {
-  return (
-    <div style={{ backgroundColor: 'black' }}>
-      <IconButton buttonType="like20" isActive={true} onMap={false} />
-      <IconButton buttonType="save" isActive={true} onMap={false} />
-      <IconButton buttonType="like36" isActive={true} onMap={false} />
-      <IconButton buttonType="like36" isActive={false} onMap={false} />
-    </div>
-  );
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
