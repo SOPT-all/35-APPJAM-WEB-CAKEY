6d4c12ea3d9ca665871452b602e7eddacaa21575
zzz-myam <jiyoo0315@naver.com>
2025-01-15 07:00:41 +0900

    feat: ProfileCard 로직 조금 구현

--- src/pages/myPage/components/ProfileCard/ProfileCard.tsx
+++ src/pages/myPage/components/ProfileCard/ProfileCard.tsx
@@ -1,5 +1,10 @@
-const ProfileCard = () => {
-  return <div>ProfileCard</div>;
+interface ProfileProps {
+  isLogin: boolean;
+  username?: string;
+}
+
+const ProfileCard = ({ isLogin, username }: ProfileProps) => {
+  return <div>{isLogin ? username : '로그인이 필요해요'}</div>;
 };
 
 export default ProfileCard;
