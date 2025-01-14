4ea165379320139ac0c5a6b2cd2e64053cc8ddb9
Parkchaeyeon <ccyy1029@naver.com>
2025-01-14 20:32:14 +0900

    feat: Modal 컴포넌트 구현

---
+++ src/components/common/Modal/Modal.tsx
@@ -0,0 +1,29 @@
+import { PropsWithChildren } from 'react';
+import { createPortal } from 'react-dom';
+
+import { backdropStyle, modalStyle } from './Modal.css';
+
+export interface ModalProps {
+  variant: 'center' | 'bottom';
+  hasBackdrop?: boolean;
+  backdropClick?: () => void;
+}
+
+const potalElement = document.getElementById('modal') as HTMLElement;
+
+const Modal = ({
+  variant,
+  hasBackdrop = false,
+  backdropClick,
+  children,
+}: PropsWithChildren<ModalProps>) => {
+  return createPortal(
+    <>
+      {hasBackdrop && <div className={backdropStyle} onClick={backdropClick} />}
+      <div className={modalStyle({ variant })}>{children}</div>
+    </>,
+    potalElement
+  );
+};
+
+export default Modal;
