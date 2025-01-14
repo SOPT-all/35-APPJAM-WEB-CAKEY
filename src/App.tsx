8d6b7ad9b3230f9d6559a3cb61d081210afaed7d
Parkchaeyeon <ccyy1029@naver.com>
2025-01-14 18:40:37 +0900

    chore: 필요없는 테스트 코드 제거, height 속성 추가 및 img 태그에 width, height 속성 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { Image } from '@components';
 import {
   authRoutes,
   designListRoutes,
@@ -30,10 +29,6 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <Image src="../public/example-img.png" width="20rem" />
-      <Image src="../public/example-img.png" width="20rem" numberLabel="1" />
-      <Image src="../public/example-img.png" width="20rem" variant="rounded" />
-      <Image src="../public/example-img.png" width="18.7rem" hasIcon />
       <div style={{ fontSize: '16px' }}>
         <ReactQueryDevtools />
       </div>
--- src/components/common/Image/Image.tsx
+++ src/components/common/Image/Image.tsx
@@ -11,22 +11,28 @@ import IconButton from '../IconButton/IconButton';
 export interface ImageProps extends HTMLAttributes<HTMLDivElement> {
   src: string;
   width: string;
+  height?: string;
   variant?: 'square' | 'rounded';
   numberLabel?: string;
   hasIcon?: boolean;
 }
 
 const Image = ({
+  src,
+  width,
+  height = width,
   variant = 'square',
   numberLabel = '',
   hasIcon = false,
-  src,
-  width,
 }: ImageProps) => {
   return (
-    <div className={divStyle} style={{ width }}>
+    <div className={divStyle} style={{ width, height }}>
       {numberLabel && <div className={numberLabelStyle}>{numberLabel}</div>}
-      <img src={src} className={imageStyle[variant]} />
+      <img
+        src={src}
+        className={imageStyle[variant]}
+        style={{ width, height }}
+      />
       {hasIcon && (
         <div className={iconButtonStyle}>
           <IconButton buttonType="like" onMap={false} isActive />
