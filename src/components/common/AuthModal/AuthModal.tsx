a670a6968fa90d1fe81fc154636d85e4b81b8bef
zzz-myam <jiyoo0315@naver.com>
2025-01-16 05:34:26 +0900

    feat: authModal 구현

--- src/components/common/AuthModal/AuthModal.tsx
+++ src/components/common/AuthModal/AuthModal.tsx
@@ -1,5 +1,32 @@
-const AuthModal = () => {
-  return <div>AuthModal</div>;
+import { authModalContainer, buttonSet, modalMessage } from './AuthModal.css';
+import TextButton from '../TextButton/TextButton';
+
+interface AuthModalProps {
+  modalText: string;
+  closeButtonText: string;
+  authActionButtonText: string;
+  onClose: () => void;
+}
+
+const AuthModal = ({
+  modalText,
+  closeButtonText,
+  authActionButtonText,
+  onClose,
+}: AuthModalProps) => {
+  return (
+    <div className={authModalContainer}>
+      <h1 className={modalMessage}>{modalText}</h1>
+      <div className={buttonSet}>
+        <TextButton size={'small'} color={'gray'} onClick={onClose}>
+          {closeButtonText}
+        </TextButton>
+        <TextButton size={'small'} color={'red'}>
+          {authActionButtonText}
+        </TextButton>
+      </div>
+    </div>
+  );
 };
 
 export default AuthModal;
