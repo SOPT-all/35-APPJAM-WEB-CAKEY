ed77ec7afa25ba23adf6092de4f05155daaa06b4
zzz-myam <jiyoo0315@naver.com>
2025-01-16 00:52:46 +0900

    chore: 코드리뷰 반영

--- src/pages/myPage/components/LetsGoButton/LetsGoButton.css.ts
+++ src/pages/myPage/components/LetsGoButton/LetsGoButton.css.ts
@@ -4,7 +4,7 @@ import { flexGenerator } from '@styles/generator.css';
 import { vars } from '@styles/theme.css';
 
 export const letsGoContainer = style([
-  flexGenerator('row', 'space-between', 'center'),
+  flexGenerator('row', 'space-between'),
   {
     padding: '1.8rem',
     cursor: 'pointer',
