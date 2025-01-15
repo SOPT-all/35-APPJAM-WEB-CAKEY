77c20b48965bafc3d6e9f14d8052aa3d631f399f
Parkchaeyeon <ccyy1029@naver.com>
2025-01-16 07:36:32 +0900

    refactor: 코드리뷰 반영

--- src/pages/designList/components/CategoryBar/CategoryBar.css.ts
+++ src/pages/designList/components/CategoryBar/CategoryBar.css.ts
@@ -3,9 +3,8 @@ import { style } from '@vanilla-extract/css';
 import { flexGenerator } from '@styles/generator.css';
 
 export const categoryBarContainer = style([
-  flexGenerator(),
+  flexGenerator('row', 'space-between'),
   {
     width: '100%',
-    gap: '1.6rem',
   },
 ]);
--- src/pages/home/page/HomePage/HomePage.tsx
+++ src/pages/home/page/HomePage/HomePage.tsx
@@ -1,35 +1,5 @@
-import { useState } from 'react';
-
-import { CATEGORY, SUB_CATEGORY } from '@constants';
-import CategoryBar from '@pages/designList/components/CategoryBar/CategoryBar';
-import SubCategoryBar from '@pages/designList/components/SubCategoryBar/SubCategoryBar';
-
-import { CategoryType, SubCategoryType } from '@types';
-
 const HomePage = () => {
-  const [selectedCategory, setSelectedCategory] =
-    useState<CategoryType>('BIRTH');
-  const [selectedSubCategory, setSelectedSubCategory] =
-    useState<SubCategoryType>('ALL');
-
-  const handleCategoryChange = (category: CategoryType) => {
-    setSelectedCategory(category);
-  };
-
-  const handleSubCategoryChange = (category: SubCategoryType) => {
-    setSelectedSubCategory(category);
-  };
-
-  return (
-    <div>
-      <CategoryBar
-        categories={CATEGORY}
-        selectedCategory={selectedCategory}
-        onCategoryChange={handleCategoryChange}
-      />
-      <SubCategoryBar categories={SUB_CATEGORY} selectedSubCategory={selectedSubCategory} onSubCategoryChange={handleSubCategoryChange} />
-    </div>
-  );
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
