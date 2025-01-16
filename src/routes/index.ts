487bbe2ffcb9d2c0aa6a91a42705d72ebb6e8550
thisishwarang <101498590+thisishwarang@users.noreply.github.com>
2025-01-16 18:21:42 +0900

    [Fix/#107] 라우트 import문 순서 에러 수정 (#108)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,11 @@ import authRoutes from './authRoutes';
 import designListRoutes from './designListRoutes';
 import homeRoutes from './homeRoutes';
 import myPageRoutes from './myPageRoutes';
-import pageRoutes from './pageRoutes';
-import routePath from './routePath';
 import storeRoutes from './storeRoutes';
 import viewRoutes from './viewRoutes';
+
+import pageRoutes from './pageRoutes';
+import routePath from './routePath';
 
 export {
   authRoutes,
