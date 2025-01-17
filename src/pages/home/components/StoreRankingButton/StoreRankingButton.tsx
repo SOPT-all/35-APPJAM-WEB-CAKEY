50471818f1a50f431907d6656ecce78357d97e68
zzz-myam <jiyoo0315@naver.com>
2025-01-17 19:41:23 +0900

    setting: 스토어 랭킹 버튼 컴포넌트 개발 세팅

---
+++ src/pages/home/components/StoreRankingButton/StoreRankingButton.tsx
@@ -0,0 +1,17 @@
+import { Label } from '@components';
+
+import { IcArrowRight20 } from '@svgs';
+
+const StoreRankingButton = () => {
+  return (
+    <div>
+      <Label />
+      <h2>서희네 앙큼가게</h2>
+      <p>1.3만</p>
+      <Label />
+      <IcArrowRight20 />
+    </div>
+  );
+};
+
+export default StoreRankingButton;
