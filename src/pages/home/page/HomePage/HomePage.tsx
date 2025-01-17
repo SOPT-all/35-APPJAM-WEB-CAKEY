d60c8eccf03be1ec866e1c8b44caf482b0185aeb
Parkchaeyeon <ccyy1029@naver.com>
2025-01-17 18:41:29 +0900

    feat: CategoryCard 속 내용을 CATEGORY map 돌려서 구현

--- src/pages/home/page/HomePage/HomePage.tsx
+++ src/pages/home/page/HomePage/HomePage.tsx
@@ -1,10 +1,15 @@
+import { CATEGORY, CATEGORY_COMPONENT } from '@constants';
 import CategoryCard from '@pages/home/components/CategoryCard/CategoryCard';
 
 const HomePage = () => {
   return (
-    <div style={{ display: 'flex', gap: '1.6rem' }}>
-      <CategoryCard category="생일" hashtag={['#생일축하', '#환갑']} />
-      <CategoryCard category="기념일" hashtag={['#결혼기념일', '#연인']} />
+    <div>
+      {CATEGORY.map((category) => (
+        <CategoryCard
+          category={CATEGORY_COMPONENT[category].text}
+          hashtag={CATEGORY_COMPONENT[category].hashtag}
+        />
+      ))}
     </div>
   );
 };
