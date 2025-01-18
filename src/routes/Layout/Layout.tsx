f3235ecb9c305c66afee37d49003da51b4370309
Parkchaeyeon <ccyy1029@naver.com>
2025-01-19 06:33:13 +0900

    feat: 파비콘 바꾸기, scrollTop 0 되도록 설정

--- src/routes/Layout/Layout.tsx
+++ src/routes/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 
 import { Header } from '@components';
+import useScrollTop from 'src/hooks/useScrollTop';
 
 import { spacing } from './Layout.css';
 
 const Layout = () => {
+  useScrollTop();
   return (
     <>
       <Header />
