8ec886275f471947471625ca1dc38aec9b23e404
thisishwarang <101498590+thisishwarang@users.noreply.github.com>
2025-01-21 06:26:13 +0900

    [Feat/#138] 카카오로그인 구현 완료 (#145)
    * fix: 로그인 코드 수정
    
    * fix: queryKey, 경로 위치 이동
    
    * fix: 코드 수정 중
    
    * fix: kakaoLogin 성공
    
    * fix: body에 redirectUri 넣기로 수정
    
    * fix: 코드리뷰 반영

---
+++ src/apis/auth/usePostKakaoLogin.ts
@@ -0,0 +1,51 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+
+import { post } from '@apis/instance';
+
+import { END_POINT, queryKey } from '@constants';
+import { useEasyNavigate } from '@hooks';
+
+import { ApiResponseType, LoginSuccessResponse } from '@types';
+
+const postKakaoLogin = async (
+  authCode: string
+): Promise<AxiosResponse<ApiResponseType<LoginSuccessResponse>>> => {
+  const response = await post<ApiResponseType<LoginSuccessResponse>>(
+    END_POINT.KAKAO_LOGIN,
+    {
+      socialType: 'KAKAO',
+      redirectUri: import.meta.env.VITE_REDIRECT_URI,
+    },
+    {
+      headers: {
+        Authorization: authCode,
+      },
+    }
+  );
+
+  return response;
+};
+
+export const usePostKakaoLogin = () => {
+  const queryClient = useQueryClient();
+  const { goHomePage } = useEasyNavigate();
+
+  return useMutation({
+    mutationFn: (authCode: string) => postKakaoLogin(authCode),
+    onSuccess: (response) => {
+      const resData = response.data.data;
+      if (resData) {
+        const { userId, userName } = resData;
+        const user = { userId, userName };
+        localStorage.setItem('user', JSON.stringify(user));
+
+        queryClient.invalidateQueries({ queryKey: [queryKey.KAKAO_LOGIN] });
+        goHomePage();
+      }
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+};
