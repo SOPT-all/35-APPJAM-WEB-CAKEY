063fa6e80f67b1de354b9c6e9dac55533b66f7b9
Parkchaeyeon <ccyy1029@naver.com>
2025-01-21 03:59:59 +0900

    feat: errorboundary, suspense 설정

--- src/routes/Layout/Layout.tsx
+++ src/routes/Layout/Layout.tsx
@@ -1,7 +1,13 @@
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Outlet, useLocation } from 'react-router-dom';
 
 import { Header } from '@components';
+import Loading from '@pages/error/Loading';
 import useScrollTop from 'src/hooks/useScrollTop';
+
+import { Error } from '@pages/error';
 
 import { spacing } from './Layout.css';
 import routePath from '../routePath';
@@ -15,10 +21,18 @@ const Layout = () => {
 
   return (
     <>
-      {!isLoginPage && (isViewPage ? <Header bgColor="yellow" /> : <Header />)}
-
-      <div className={spacing} />
-      <Outlet />
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary onReset={reset} FallbackComponent={Error}>
+            <Suspense fallback={<Loading />}>
+              {!isLoginPage &&
+                (isViewPage ? <Header bgColor="yellow" /> : <Header />)}
+              <div className={spacing} />
+              <Outlet />
+            </Suspense>
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
     </>
   );
 };
