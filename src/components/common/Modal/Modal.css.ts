65ec602696242f4197e9f94f891544724d189d93
Parkchaeyeon <ccyy1029@naver.com>
2025-01-24 04:46:37 +0900

    feat: center 모달 z-index 다시 설정하기

--- src/components/common/Modal/Modal.css.ts
+++ src/components/common/Modal/Modal.css.ts
@@ -1,21 +1,28 @@
-import { style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
 
 import { fixedGenerator, flexGenerator } from '@styles/generator.css';
 import { vars } from '@styles/theme.css';
 
-export const backdropStyle = style([
-  fixedGenerator({ top: 0, center: true }, 3),
-  {
-    height: '100%',
-    background: vars.colors.dimmed,
-  },
-]);
+export const backdropStyle = recipe({
+  base: [
+    fixedGenerator({ top: 0, center: true }),
+    {
+      height: '100%',
+      background: vars.colors.dimmed,
+    },
+  ],
+  variants: {
+    variant: {
+      center: {zIndex: 5},
+      bottom: {zIndex: 3}
+    }
+  }
+})
 
 export const modalStyle = recipe({
   base: [
     flexGenerator(),
-    { zIndex: 4, backgroundColor: vars.colors.white, position: 'fixed' },
+    {backgroundColor: vars.colors.white, position: 'fixed' },
   ],
   variants: {
     variant: {
@@ -24,12 +31,14 @@ export const modalStyle = recipe({
         left: '50%',
         transform: 'translate(-50%, -50%)',
         borderRadius: '10px',
+        zIndex: 5, 
       },
       bottom: [
         fixedGenerator({ bottom: 0, center: true }, 4),
         {
           borderRadius: '10px 10px 0px 0px',
           boxShadow: '0px 0px 20px 0px rgba(0, 0, 0, 0.10)',
+          zIndex: 4
         },
       ],
     },
