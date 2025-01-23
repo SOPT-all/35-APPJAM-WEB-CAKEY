72092f2ffdcfb91b1e4fcd69ed548efa26fef226
youtheyeon <yuseoyeon@ewhain.net>
2025-01-23 20:06:37 +0900

    feat: 스토어 페이지 404 에러 처리

--- src/apis/store/useFetchStoreLink.ts
+++ src/apis/store/useFetchStoreLink.ts
@@ -6,16 +6,12 @@ import { END_POINT, queryKey } from '@constants';
 
 import { ApiResponseType, StoreLinkResponse } from '@types';
 
-const fetchStoreLink = async (storeId: number): Promise<StoreLinkResponse> => {
-  try {
-    const response = await instance.get<ApiResponseType<StoreLinkResponse>>(
-      END_POINT.FETCH_STORE_LINK(storeId)
-    );
-    return response.data.data;
-  } catch (error) {
-    console.error();
-    throw error;
-  }
+const fetchStoreLink = async (storeId: number): Promise<string> => {
+  const response = await instance.get<ApiResponseType<StoreLinkResponse>>(
+    END_POINT.FETCH_STORE_LINK(storeId)
+  );
+  if (!response.data) return '';
+  return response.data.data.kakaoLink;
 };
 
 export const useFetchStoreLink = (storeId: number) => {
