e512d16a12ed9eac3a246f1440155ec81fdf9997
Parkchaeyeon <ccyy1029@naver.com>
2025-03-11 15:34:03 +0900

    feat: 지하철역 검색 기능 구현

--- src/pages/view/components/SelectStationModal/SelectStationModal.tsx
+++ src/pages/view/components/SelectStationModal/SelectStationModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Header, Input, TextButton } from '@components';
 
@@ -26,7 +26,23 @@ const SelectStationModal = ({
   onCurrentLocationChange,
   onClose,
 }: SelectStationModalProps) => {
+  const [searchedStation, setSearchedStation] = useState('');
+  const [debouncedStation, setDebouncedStation] = useState(searchedStation);
   const [selectedLocation, setSelectedLocation] = useState(currentLocation);
+
+  const handleChangeStationInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchedStation(e.target.value);
+    console.log(filteredStation);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedStation(searchedStation), 500);
+    return () => clearTimeout(timeout);
+  }, [searchedStation]);
+
+  const filteredStation = stationKrNames.filter((stations) =>
+    stations.includes(debouncedStation)
+  );
 
   const handleLocationClick = (location: string) => {
     setSelectedLocation(location);
@@ -46,18 +62,39 @@ const SelectStationModal = ({
           <span className={spanStyle}>아직 등록되지 않은 지역이 있어요!</span>
         </section>
         <section className={inputSection}>
-          <Input.InputField />
+          <Input.InputField
+            value={searchedStation}
+            onChange={handleChangeStationInput}
+            placeholder="지하철 역을 검색해주세요"
+          />
         </section>
-        <section className={scrollSection}>
-          {stationKrNames.map((station, index) => (
-            <LocationSelectButton
-              key={index}
-              location={station}
-              currentLocation={selectedLocation}
-              onClick={() => handleLocationClick(station)}
-            />
-          ))}
-        </section>
+        {searchedStation === '' ? (
+          <section className={scrollSection}>
+            {stationKrNames.map((station, index) => (
+              <LocationSelectButton
+                key={index}
+                location={station}
+                currentLocation={selectedLocation}
+                onClick={() => handleLocationClick(station)}
+              />
+            ))}
+          </section>
+        ) : (
+          <section className={scrollSection}>
+            {filteredStation?.length
+              ? 
+              filteredStation.map((station, index) => (
+                  <LocationSelectButton
+                    key={index}
+                    location={station}
+                    currentLocation={selectedLocation}
+                    onClick={() => handleLocationClick(station)}
+                  />
+                ))
+              : '검색 결과가 없습니다.'}
+          </section>
+        )}
+
         <section className={footerSection}>
           <TextButton size="xLarge" onClick={handleSelectButtonClick}>
             선택하기
